2e4a36fb092b20e42facbd9882e9f26eeead97cd
gigakuma <qiang.l.x@gmail.com>
2020-08-15 18:51:10 +0800

    initial

---
+++ main.py
@@ -0,0 +1,35 @@
+import asyncio
+import nest_asyncio
+from tomon_sdk import bot
+
+nest_asyncio.apply()
+
+bot_app = None
+
+
+def on_dispatch(data):
+    loop.run_until_complete(speak(data))
+
+
+async def speak(data):
+    e = data.get('e')
+    d = data.get('d')
+    if e == 'MESSAGE_CREATE':
+        if d.get('author').get('id') != bot_app.id:
+            if d.get('content') == '/ping':
+                channel_id = d.get('channel_id')
+                payload = {}
+                payload['content'] = 'pong'
+                await bot_app.api().route('/channels/{}/messages'.format(channel_id)).post(data=payload)
+
+
+if __name__ == "__main__":
+    bot_app = bot.Bot()
+
+    async def main():
+        bot_app.on(bot.OpCodeEvent.DISPATCH, on_dispatch)
+        await bot_app.start('your.bot.token')
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    print("finish")
